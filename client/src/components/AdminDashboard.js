7557ef1c5024b4819d277ff8b475903b0780f218
Sumuditha Lansakara <sumudithalanz@gmail.com>
2024-04-03 20:47:13 +0530

    updated admin dashboard with projects route

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -4,9 +4,13 @@ import { Link } from "react-router-dom";
 const AdminDashboard = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <Link to="/admin/blogs" className="btn btn-primary">
+      <Link to="/admin/blogs" className="btn btn-primary mr-3">
         Manage Blogs
       </Link>
+      <Link to="/admin/projects" className="btn btn-primary">
+        Manage Projects
+      </Link>
+
     </div>
   );
 };
